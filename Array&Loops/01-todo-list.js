a9db56ded45e845e8b4975aecc0d5c4832357dbd
nirajmgr1010 <neerazmgr9@gmail.com>
2026-04-24 23:09:35 +0545

    To Do List Changes

--- Array&Loops/01-todo-list.js
+++ Array&Loops/01-todo-list.js
@@ -1,7 +1,6 @@
 const todoList = [{
     name: 'make dinner', 
     dueDate: '2022-12-22',
-    name: 'wash dishes',
     },
    {
     name: 'wash dishes',
---
+++ ProjectPractice/To-do-Script.js
@@ -0,0 +1,58 @@
+const TodoList = [{
+    id: 0,
+    name: 'Default',
+    PhoneNo: 0,
+    Course: 'Default'
+}
+];
+renderTodoList();
+function renderTodoList(){
+    let todoListHtml = '';
+    for(let i =0; i<TodoList.length; i++){
+        const TodoObject = TodoList[i];
+        const {id,name,PhoneNo,Course} = TodoObject;
+
+        const Html = `
+           <div>${id}</div>
+           <div>${name}</div>
+           <div>${PhoneNo}</div>
+           <div>${Course}</div>
+           <div><button onclick='deleteTodo(${i})'>Delete</button></div>
+        `;
+         todoListHtml += Html;
+    }
+
+    document.querySelector('.js-todo-list').innerHTML = todoListHtml;
+
+}
+function deleteTodo(index){
+    TodoList.splice(index, 1);
+    renderTodoList();
+}
+function addTodo (){
+    const InputId = document.querySelector('#Id');
+    const Id = Number(InputId.value);
+
+    const InputName = document.querySelector('#name');
+    const Name = InputName.value;
+
+    const InputPhone = document.querySelector('#Contact');
+    const Phone = Number(InputPhone.value);
+
+    const InputCourse = document.querySelector('#Course');
+    const Course = InputCourse.value;
+
+    TodoList.push({
+       id: Id,
+       name: Name,
+       PhoneNo: Phone,
+       Course: Course
+    });
+    
+    InputId.value = '';
+    InputName.value = '';
+    InputPhone.value = '';
+    InputCourse.value = '';
+    renderTodoList();
+
+}
